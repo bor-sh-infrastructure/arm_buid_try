901f2d194ad0dd9b7cdda95573ba382bb867faef
b-sh <b-sh@gmx.net>
2015-07-29 21:23:59 +0200

    uncomment svn temporary

--- .prepack.sh
+++ .prepack.sh
@@ -9,5 +9,5 @@ wget --no-check-certificate https://googledrive.com/host/0B0AeTm15hbhXUksyQXlKaE
 sudo tar xjf arm-chroot.tar.bz -C /
 sudo chroot ${CHROOT_DIR} bash -c "uname -a"
 sudo chroot ${CHROOT_DIR} bash -c "g++ --version"
-sudo chroot ${CHROOT_DIR} bash -c "cd /home/user/arm_buid_try/google-breakpad-read-only && svn up"
+#sudo chroot ${CHROOT_DIR} bash -c "cd /home/user/arm_buid_try/google-breakpad-read-only && svn up"
 sudo chroot ${CHROOT_DIR} bash -c "cd /home/user/arm_buid_try/google-breakpad-read-only && ./configure && { make -j4 check; RET=$?; } && cat ./test-suite.log && exit $RET"
